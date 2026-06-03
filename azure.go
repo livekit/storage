d499771c3b88353a18e5f3213d27506e76a2bf78
David Colburn <xero73@gmail.com>
2026-06-03 12:35:31 -0400

    replace outdated azure sdk (#28)

--- azure.go
+++ azure.go
@@ -23,66 +23,58 @@ import (
 	"path"
 	"time"
 
-	"github.com/Azure/azure-storage-blob-go/azblob"
+	"github.com/Azure/azure-sdk-for-go/sdk/azcore/to"
+	"github.com/Azure/azure-sdk-for-go/sdk/storage/azblob"
+	"github.com/Azure/azure-sdk-for-go/sdk/storage/azblob/blob"
+	"github.com/Azure/azure-sdk-for-go/sdk/storage/azblob/sas"
+	"github.com/Azure/azure-sdk-for-go/sdk/storage/azblob/service"
 )
 
+const azureBlockSize = 4 * 1024 * 1024
+const azureParallelism = 16
+
 type azureBLOBStorage struct {
-	conf         *AzureConfig
-	containerUrl azblob.ContainerURL
-	serviceUrl   azblob.ServiceURL
+	conf       *AzureConfig
+	client     *azblob.Client
+	serviceUrl string
 }
 
 func NewAzure(conf *AzureConfig) (Storage, error) {
-	credential, err := azblob.NewSharedKeyCredential(
-		conf.AccountName,
-		conf.AccountKey,
-	)
+	cred, err := azblob.NewSharedKeyCredential(conf.AccountName, conf.AccountKey)
 	if err != nil {
 		return nil, err
 	}
 
-	pipeline := azblob.NewPipeline(credential, azblob.PipelineOptions{
-		Retry: azblob.RetryOptions{
-			Policy:        azblob.RetryPolicyExponential,
-			MaxTries:      5,
-			MaxRetryDelay: time.Second * 5,
-		},
-	})
+	serviceUrl := fmt.Sprintf("https://%s.blob.core.windows.net/", conf.AccountName)
+	client, err := azblob.NewClientWithSharedKeyCredential(serviceUrl, cred, nil)
+	if err != nil {
+		return nil, err
+	}
 
-	host := fmt.Sprintf("%s.blob.core.windows.net", conf.AccountName)
 	return &azureBLOBStorage{
-		conf: conf,
-		serviceUrl: azblob.NewServiceURL(url.URL{
-			Scheme: "https",
-			Host:   host,
-		}, pipeline),
-		containerUrl: azblob.NewContainerURL(url.URL{
-			Scheme: "https",
-			Host:   host,
-			Path:   conf.ContainerName,
-		}, pipeline),
+		conf:       conf,
+		client:     client,
+		serviceUrl: serviceUrl,
 	}, nil
 }
 
 func (s *azureBLOBStorage) location(storagePath string) string {
 	return (&url.URL{
 		Scheme: "https",
-		Host:   s.conf.AccountName + ".blob.core.windows.net",
+		Host:   fmt.Sprintf("%s.blob.core.windows.net", s.conf.AccountName),
 		Path:   path.Join(s.conf.ContainerName, storagePath),
 	}).String()
 }
 
 func (s *azureBLOBStorage) UploadData(data []byte, storagePath, contentType string) (string, int64, error) {
-	blobUrl := s.containerUrl.NewBlockBlobURL(storagePath)
-	_, err := azblob.UploadBufferToBlockBlob(context.Background(), data, blobUrl, azblob.UploadToBlockBlobOptions{
-		BlobHTTPHeaders: azblob.BlobHTTPHeaders{ContentType: contentType},
-		BlockSize:       4 * 1024 * 1024,
-		Parallelism:     16,
+	_, err := s.client.UploadBuffer(context.Background(), s.conf.ContainerName, storagePath, data, &azblob.UploadBufferOptions{
+		HTTPHeaders: &blob.HTTPHeaders{BlobContentType: &contentType},
+		BlockSize:   azureBlockSize,
+		Concurrency: azureParallelism,
 	})
 	if err != nil {
 		return "", 0, err
 	}
-
 	return s.location(storagePath), int64(len(data)), nil
 }
 
@@ -100,13 +92,10 @@ func (s *azureBLOBStorage) UploadFile(filepath, storagePath, contentType string)
 		return "", 0, err
 	}
 
-	// upload blocks in parallel for optimal performance
-	// it calls PutBlock/PutBlockList for files larger than 256 MBs and PutBlob for smaller files
-	blobUrl := s.containerUrl.NewBlockBlobURL(storagePath)
-	_, err = azblob.UploadFileToBlockBlob(context.Background(), file, blobUrl, azblob.UploadToBlockBlobOptions{
-		BlobHTTPHeaders: azblob.BlobHTTPHeaders{ContentType: contentType},
-		BlockSize:       4 * 1024 * 1024,
-		Parallelism:     16,
+	_, err = s.client.UploadFile(context.Background(), s.conf.ContainerName, storagePath, file, &azblob.UploadFileOptions{
+		HTTPHeaders: &blob.HTTPHeaders{BlobContentType: &contentType},
+		BlockSize:   azureBlockSize,
+		Concurrency: azureParallelism,
 	})
 	if err != nil {
 		return "", 0, err
@@ -118,17 +107,18 @@ func (s *azureBLOBStorage) UploadFile(filepath, storagePath, contentType string)
 func (s *azureBLOBStorage) ListObjects(prefix string) ([]string, error) {
 	var objects []string
 
-	for marker := (azblob.Marker{}); marker.NotDone(); {
-		listBlob, err := s.containerUrl.ListBlobsFlatSegment(context.Background(), marker, azblob.ListBlobsSegmentOptions{
-			Prefix: prefix,
-		})
+	pager := s.client.NewListBlobsFlatPager(s.conf.ContainerName, &azblob.ListBlobsFlatOptions{
+		Prefix: &prefix,
+	})
+	for pager.More() {
+		page, err := pager.NextPage(context.Background())
 		if err != nil {
 			return nil, err
 		}
-
-		marker = listBlob.NextMarker
-		for _, blobInfo := range listBlob.Segment.BlobItems {
-			objects = append(objects, blobInfo.Name)
+		for _, item := range page.Segment.BlobItems {
+			if item.Name != nil {
+				objects = append(objects, *item.Name)
+			}
 		}
 	}
 
@@ -136,26 +126,26 @@ func (s *azureBLOBStorage) ListObjects(prefix string) ([]string, error) {
 }
 
 func (s *azureBLOBStorage) DownloadData(storagePath string) ([]byte, error) {
-	blobUrl := s.containerUrl.NewBlobURL(storagePath)
+	ctx := context.Background()
+	blobClient := s.client.ServiceClient().NewContainerClient(s.conf.ContainerName).NewBlobClient(storagePath)
 
-	props, err := blobUrl.GetProperties(context.Background(), azblob.BlobAccessConditions{}, azblob.ClientProvidedKeyOptions{})
+	props, err := blobClient.GetProperties(ctx, nil)
 	if err != nil {
 		return nil, err
 	}
+	if props.ContentLength == nil {
+		return nil, errors.New("azure: missing content length")
+	}
 
-	b := make([]byte, props.ContentLength())
-	err = azblob.DownloadBlobToBuffer(context.Background(), blobUrl, 0, azblob.CountToEnd, b, azblob.DownloadFromBlobOptions{
-		BlockSize:   4 * 1024 * 1024,
-		Parallelism: 16,
-		RetryReaderOptionsPerBlock: azblob.RetryReaderOptions{
-			MaxRetryRequests: 3,
-		},
+	buf := make([]byte, *props.ContentLength)
+	_, err = s.client.DownloadBuffer(ctx, s.conf.ContainerName, storagePath, buf, &azblob.DownloadBufferOptions{
+		BlockSize:   azureBlockSize,
+		Concurrency: azureParallelism,
 	})
 	if err != nil {
 		return nil, err
 	}
-
-	return b, nil
+	return buf, nil
 }
 
 func (s *azureBLOBStorage) DownloadFile(filepath, storagePath string) (int64, error) {
@@ -165,13 +155,9 @@ func (s *azureBLOBStorage) DownloadFile(filepath, storagePath string) (int64, er
 	}
 	defer file.Close()
 
-	blobUrl := s.containerUrl.NewBlobURL(storagePath)
-	err = azblob.DownloadBlobToFile(context.Background(), blobUrl, 0, 0, file, azblob.DownloadFromBlobOptions{
-		BlockSize:   4 * 1024 * 1024,
-		Parallelism: 16,
-		RetryReaderOptionsPerBlock: azblob.RetryReaderOptions{
-			MaxRetryRequests: 3,
-		},
+	_, err = s.client.DownloadFile(context.Background(), s.conf.ContainerName, storagePath, file, &azblob.DownloadFileOptions{
+		BlockSize:   azureBlockSize,
+		Concurrency: azureParallelism,
 	})
 	if err != nil {
 		return 0, err
@@ -189,32 +175,37 @@ func (s *azureBLOBStorage) GeneratePresignedUrl(storagePath string, expiration t
 		return "", errors.New("OAuth required")
 	}
 
-	now := time.Now()
+	now := time.Now().UTC()
 	exp := now.Add(expiration)
 
-	serviceUrl := s.serviceUrl.WithPipeline(azblob.NewPipeline(s.conf.TokenCredential, azblob.PipelineOptions{}))
-	udc, err := serviceUrl.GetUserDelegationCredential(
-		context.Background(), azblob.NewKeyInfo(now, exp), nil, nil,
-	)
+	svcClient, err := service.NewClient(s.serviceUrl, s.conf.TokenCredential, nil)
 	if err != nil {
 		return "", err
 	}
 
-	qp, err := azblob.BlobSASSignatureValues{
-		Protocol:      azblob.SASProtocolHTTPS,
+	udc, err := svcClient.GetUserDelegationCredential(context.Background(), service.KeyInfo{
+		Start:  to.Ptr(now.Format(sas.TimeFormat)),
+		Expiry: to.Ptr(exp.Format(sas.TimeFormat)),
+	}, nil)
+	if err != nil {
+		return "", err
+	}
+
+	qp, err := sas.BlobSignatureValues{
+		Protocol:      sas.ProtocolHTTPS,
 		StartTime:     now,
 		ExpiryTime:    exp,
-		Permissions:   azblob.AccountSASPermissions{Read: true}.String(),
+		Permissions:   (&sas.BlobPermissions{Read: true}).String(),
 		ContainerName: s.conf.ContainerName,
 		BlobName:      storagePath,
-	}.NewSASQueryParameters(udc)
+	}.SignWithUserDelegation(udc)
 	if err != nil {
 		return "", err
 	}
 
 	loc := &url.URL{
 		Scheme:   "https",
-		Host:     s.conf.AccountName + ".blob.core.windows.net",
+		Host:     fmt.Sprintf("%s.blob.core.windows.net", s.conf.AccountName),
 		Path:     path.Join(s.conf.ContainerName, storagePath),
 		RawQuery: qp.Encode(),
 	}
@@ -222,14 +213,13 @@ func (s *azureBLOBStorage) GeneratePresignedUrl(storagePath string, expiration t
 }
 
 func (s *azureBLOBStorage) DeleteObject(storagePath string) error {
-	blobUrl := s.containerUrl.NewBlobURL(storagePath)
-	_, err := blobUrl.Delete(context.Background(), azblob.DeleteSnapshotsOptionNone, azblob.BlobAccessConditions{})
+	_, err := s.client.DeleteBlob(context.Background(), s.conf.ContainerName, storagePath, nil)
 	return err
 }
 
 func (s *azureBLOBStorage) DeleteObjects(storagePaths []string) error {
-	for _, path := range storagePaths {
-		if err := s.DeleteObject(path); err != nil {
+	for _, p := range storagePaths {
+		if err := s.DeleteObject(p); err != nil {
 			return err
 		}
 	}
